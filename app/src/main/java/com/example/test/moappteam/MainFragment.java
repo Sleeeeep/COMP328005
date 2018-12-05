213a0c5166c79ddd135b9c4da7949b90803299d8
Sunik9303 <attmey@naver.com>
2018-12-05 17:11:20 +0900

    로그인 화면 수정 및 메인프레그먼트 구성 요소 추가

--- app/src/main/java/com/example/test/moappteam/MainFragment.java
+++ app/src/main/java/com/example/test/moappteam/MainFragment.java
@@ -1,11 +1,14 @@
 package com.example.test.moappteam;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -23,7 +26,24 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_main,container,false);
+
+        Button button = (Button) view.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getActivity(),SignUPActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        return view;
+        //return inflater.inflate(R.layout.fragment_main, container, false);
+
     }
 
 }
--- app/src/main/java/com/example/test/moappteam/SignUPActivity.java
+++ app/src/main/java/com/example/test/moappteam/SignUPActivity.java
@@ -3,6 +3,11 @@ package com.example.test.moappteam;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SignUPActivity extends AppCompatActivity {
 
@@ -17,5 +22,35 @@ public class SignUPActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        Spinner spinner = (Spinner)findViewById(R.id.favorSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.favor,
+                android.R.layout.simple_spinner_dropdown_item );
+                //android.R.layout.simple_spinner_item );
+
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        Log.i("MainActivity.spinner", "pos = " + position + ", id = " + id);
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+                        Log.i("MainActivity.spinner", "선택 없음");
+                    }
+                }
+        );
     }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
 }
